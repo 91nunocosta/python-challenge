630ddc73c370bb35ad5c3303ede5b51efb8413d2
nunocosta <91nunocosta@gmail.com>
2018-07-07 16:31:59 +0100

    stubs Autocomplete System

---
+++ autocomplete.py
@@ -0,0 +1,11 @@
+"""Autocomplete system. Suggests word completitions from a preconfigured corpus."""
+
+class Autocompleter():
+
+    def __init__(self, words):
+        """Initialize a autocompleter with a given set of words."""
+        pass
+
+    def suggest(self, prefix):
+        """Return all words in the corpus starting with a givne prefix."""
+        pass
