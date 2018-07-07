b4826e3c78c9f58784230043d12b894281c15557
nunocosta <91nunocosta@gmail.com>
2018-07-07 17:56:26 +0100

    create tests for autocomplete suggestions

---
+++ tests/test_autocomplete.py
@@ -0,0 +1,26 @@
+"""Test Autocomplete System."""
+from unittest import TestCase
+from autocomplete import Autocompleter
+
+class TestAutocompleter(TestCase):
+    def setUp(self):
+        self.autocompleter = Autocompleter([
+            'WhatsApp Messenger',
+            'Facebook',
+            'Facebook Lite'
+        ])
+
+    def test_can_suggest(self):
+        """Test can suggest all words starting with a given prefix."""
+        suggestions = self.autocompleter.suggest('Fac')
+        expected_suggestions = [
+            'Facebook',
+            'Facebook Lite'
+        ]
+        self.assertListEqual(list(suggestions), expected_suggestions)
+
+    def test_can_suggest_no_words(self):
+        """Test can doesn't suggest any word when none starts with the given prefix"""
+        suggestions = self.autocompleter.suggest('PrefixForNothing')
+        expected_suggestions = []
+        self.assertListEqual(list(suggestions), expected_suggestions)
