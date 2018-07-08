cc0f18041cf81f44850e8c980eb8e0b501b534ce
nunocosta <91nunocosta@gmail.com>
2018-07-08 02:43:14 +0100

    can get suggestions

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,12 +1,23 @@
 """API definition."""
+import os
 from flask import Flask
+from flask import request
 from flask import jsonify
+from autocomplete import Autocompleter 
 
-def create_app():
+def create_app(corpus_path=None):
     app = Flask(__name__)
+
+    if corpus_path is None:
+        corpus_path = os.getenv('CORPUS_PATH')
+
+    with open(corpus_path) as corpus_file:
+        autocompleter = Autocompleter(word.rstrip('\n') for word in corpus_file)
 
     @app.route('/suggestions')
     def suggestions():
-        return jsonify(results=[])
+        prefix = request.args.get('q')
+        suggestions = autocompleter.suggest(prefix)
+        return jsonify(results=list(suggestions))
 
     return app
--- flaskr/tests/test_api.py
+++ flaskr/tests/test_api.py
@@ -7,9 +7,9 @@ from flaskr import create_app
 class TestSuggestionsAPI(TestCase):
     
     def setUp(self):
-        self.app = create_app()
+        corpus_path = path.join(path.dirname(__file__), '../../test_files/190titles.csv')
+        self.app = create_app(corpus_path=corpus_path)
         self.app.config['TESTING'] = True
-        self.app.config['CORPUS_PATH'] = path.join(path.dirname(__file__), '../../test_files/190titles.csv')
         self.client = self.app.test_client()
     
     def test_can_get_suggestions(self):
